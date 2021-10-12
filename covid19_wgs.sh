25bd1a0e6564c4f1985016d9829e15728e24980f
SorenKarst <sorenkarst@gmail.com>
2021-10-12 13:28:12 +0200

    Added settings for different primer schemes and lab protocols

--- covid19_wgs.sh
+++ covid19_wgs.sh
@@ -1,37 +1,60 @@
 #!/bin/bash
 # DESCRIPTION
-#    covid19_wgs_consensus: COVID-19 WGS consensus pipeline.
+#    covid19_wgs: COVID-19 WGS consensus pipeline.
 #    
 # IMPLEMENTATION
-#    author   S?ren Karst (sorenkarst@gmail.com)
+#    author   Soren Karst (sorenkarst@gmail.com)
 #    license  GNU General Public License
+
+# Conda environments dir
+export CONDA_DIR=$(dirname $(readlink -f "$0") | sed 's|envs/covid19_wgs/covid19_wgs|envs|g')
+export SCHEME_LIST=$(
+  find \
+    $CONDA_DIR/covid19_wgs/artic-ncov2019/primer_schemes/nCoV-2019/ \
+    -maxdepth 1 \
+    -mindepth 1 \
+    -type d |\
+  sed 's|.*/||'
+)
 
 ### Description ----------------------------------------------------------------
 
 USAGE="
--- covid19_wgs: COVID-19 WGS consensus and QC pipeline.
+-- covid19_wgs: COVID-19 WGS consensus and QC pipeline for ONT data.
 
-usage: covid19_wgs [-h ] ( -i dir -o dir -b string -t integer)
+usage: covid19_wgs [-h ] ( -i dir -o dir -b string - p string -s string -t integer)
 where:
     -h   Show this help text.
     -i   Parent folder containing barcode subfolder with fastq file(s).
     -o   Output folder and working directory.
     -b   Space delimited string og used barcode IDs. IDs should match names of
-         barcode subfolders. Bash expansion possible (eg. barcode{25..32}
-         barcode{34..42}). If unused barcodes are registered, they will be
-         partially processed and flagged in the QC.
+         barcode subfolders. Bash expansion possible (eg. barcode{25..32} barcode{34..42}).
+         If data from unused barcodes are registered, they will be partially
+         processed and flagged in the QC.
+    -p   Library protocol {artic_V3, midnight_V1M, ...}.
+         artic: intact amplicons, expect raw reads to be 400-700 bp.
+         midnight: fragmented amplicons, expect raw reads to be 250-1500 bp.
+         ...: Custom library protocol in the format \"primer_scheme;length_min;length_max\"
+         eg. \"V4;250;400\"
+    -a   Optional arguments to consensus pipeline.
+         ONT default: \"--medaka --medaka-model r941_min_high_g360\"
+         ILM compatible: \"--bwa --skip-nanopolish --no-longshot\" (Only use for testing)
     -t   Number of CPU threads.
+    
+Available primer schemes: $(echo $SCHEME_LIST)
 "
 
 ### Terminal Arguments ---------------------------------------------------------
 
 # Import user arguments
-while getopts ':hzdi:o:b:t:' OPTION; do
+while getopts ':hzdi:o:b:p:at:' OPTION; do
   case $OPTION in
     h) echo "$USAGE"; exit 1;;
     i) DATA_DIR=$OPTARG;;
     o) ANALYSIS_DIR=$OPTARG;;
     b) BARCODE_SUBSET=$(eval echo "$OPTARG" | sed 's/ /|/g');;
+    p) PROTOCOL=$OPTARG;;
+    a) ARTIC_ARGS=$OPTARG;;
     t) THREADS=$OPTARG;;
     :) printf "missing argument for -$OPTARG\n" >&2; exit 1;;
     \?) printf "invalid option for -$OPTARG\n" >&2; exit 1;;
@@ -43,13 +66,36 @@ MISSING="is missing but required. Exiting."
 if [ -z ${DATA_DIR+x} ]; then echo "-i $MISSING"; echo ""; echo "$USAGE"; exit 1; fi;
 if [ -z ${ANALYSIS_DIR+x} ]; then echo "-o $MISSING"; echo ""; echo "$USAGE"; exit 1; fi;
 if [ -z ${BARCODE_SUBSET+x} ]; then echo "-b $MISSING"; echo ""; echo "$USAGE"; exit 1; fi;
+if [ -z ${PROTOCOL+x} ]; then echo "-p $MISSING"; echo ""; echo "$USAGE"; exit 1; fi;
+if [ -z ${ARTIC_ARGS+x} ]; then ARTIC_ARGS="--medaka --medaka-model r941_min_high_g360 --normalise 100"; fi;
 if [ -z ${THREADS+x} ]; then echo "-t $MISSING"; echo ""; echo "$USAGE"; exit 1; fi;
-if [ -z ${DATA_DIR+x} ]; then echo "-i $MISSING"; echo ""; echo "$USAGE"; exit 1; fi;
 
-# Conda environments dir
-export $CONDA_DIR="$(dirname "$(readlink -f "$0")" | sed 's|envs/covid19_wgs|envs/g')"
 
-# Activate conda environment
+# Setup protocol settings
+case $PROTOCOL in
+  artic_V3)
+    LEN_MIN=400
+    LEN_MAX=700
+    PRIMER_SCHEME=V3
+    ;;
+  midnight_V1)
+    LEN_MIN=250
+    LEN_MAX=1500
+    PRIMER_SCHEME=V1M
+    ;;
+  *;*;*)
+    LEN_MIN=$(echo "$PROTOCOL" | cut -d";" -f2)
+    LEN_MAX=$(echo "$PROTOCOL" | cut -d";" -f3)
+    PRIMER_SCHEME=$(echo "$PROTOCOL" | cut -d";" -f1)
+  ;;
+  *) printf "Invalid format for -p: $PROTOCOL\n" >&2
+  echo ""
+  echo "$USAGE"
+  exit 1
+  ;;
+esac
+
+# Artic version
 ARTIC_VERSION=$(artic -v)
 
 # Fastq
@@ -75,7 +121,12 @@ grep -E "$BARCODE_SUBSET" |\
 parallel \
   -j $THREADS \
   --rpl '{bid} s:.*/::' \
-  "artic guppyplex --skip-quality-check --min-length 400 --max-length 700 --directory {} --output $ANALYSIS_DIR/filter/{bid}_filt.fastq"
+  "artic guppyplex \\
+  --skip-quality-check \\
+  --min-length $LEN_MIN \\
+  --max-length $LEN_MAX \\
+  --directory {} \\
+  --output $ANALYSIS_DIR/filter/{bid}_filt.fastq"
   
 # Consensus
 mkdir $ANALYSIS_DIR/consensus
@@ -90,13 +141,12 @@ parallel \
   "mkdir $ANALYSIS_DIR/consensus/{bid};
   cd $ANALYSIS_DIR/consensus/{bid};
   artic minion \\
-  --medaka \\
-  --medaka-model r941_min_high_g360 \\
+  $ARTIC_ARGS \\
   --normalise 100 \\
   --threads 1 \\
-  --scheme-directory $CONDA_DIR/artic/artic-ncov2019/primer_schemes \\
+  --scheme-directory $CONDA_DIR/covid19_wgs/artic-ncov2019/primer_schemes \\
   --read-file {} \\
-  nCoV-2019/V3 {bid}"
+  nCoV-2019/$PRIMER_SCHEME {bid}"
 
 find \
   $ANALYSIS_DIR/consensus \
@@ -119,7 +169,7 @@ parallel \
   "
   align_trim \\
     --normalise 1000000 \\
-    $CONDA_DIR/artic/artic-ncov2019/primer_schemes/nCoV-2019/V3/nCoV-2019.scheme.bed \\
+    $CONDA_DIR/covid19_wgs/artic-ncov2019/primer_schemes/nCoV-2019/$PRIMER_SCHEME/nCoV-2019.scheme.bed \\
     --remove-incorrect-pairs \\
     --report $ANALYSIS_DIR/consensus/{sid}/{sid}.alignreport_nonormalize.txt \\
     < {} 2> \\
@@ -129,7 +179,7 @@ parallel \
   "
 
 cat \
-  $CONDA_DIR/artic/artic-ncov2019/primer_schemes/nCoV-2019/V3/nCoV-2019.insert.bed \
+  $CONDA_DIR/covid19_wgs/artic-ncov2019/primer_schemes/nCoV-2019/$PRIMER_SCHEME/nCoV-2019.insert.bed \
   > $ANALYSIS_DIR/coverage.bed
   
   
@@ -244,7 +294,12 @@ augur tree \
   --alignment $ANALYSIS_DIR/nextclade/consensus.aligned.hgmq.fasta \
   --output $ANALYSIS_DIR/consensus_tree.nwk \
   --nthreads $THREADS &> $ANALYSIS_DIR/log.out
-  
+
+# Copy QC report template
+cp \
+  $CONDA_DIR/covid19_wgs/covid19_wgs/covid19_wgs_qc.rmd \
+  $ANALYSIS_DIR/
+
 # Protocol specification file
 get_latest_release() {
   curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
@@ -253,9 +308,9 @@ get_latest_release() {
 }
 
 printf "process\tversion_used\tversion_available
-lab_protocol\tNA\tNA
-primer_scheme\tArtic V3\tNA
+lab_protocol\t$PROTOCOL($PRIMER_SCHEME;$LEN_MIN;$LEN_MAX)\tNA
 consensus_pipeline\t$ARTIC_VERSION\tartic $(get_latest_release "artic-network/fieldbioinformatics")
+consensus_arguments\t$ARTIC_ARGS\tNA)
 pangolin\t$PANGOLIN_VERSION\tpangolin $(get_latest_release "cov-lineages/pangolin")
 pangolearn\t$PANGOLEARN_VERSION\tpangoLEARN $(get_latest_release "cov-lineages/pangoLEARN")
 nextstrain-cli\t$NXST_VERSION\tNA
